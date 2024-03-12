a7a475feb6e7514f64bbf748070c3362cf75a915
skutner <sava.dumitru.daniel@gmail.com>
2024-03-12 12:36:29 +0200

    Implement session expiry and logout trigger checks
    Added a Timer.js script that constantly checks session expiry and logout trigger commands. Adjusted OauthMiddleware.js to return true or false upon checking session expiry or logout trigger. Simplified Timer.js in demiurge-wallet loader. Now the checks use fetch request instead of parsing cookies from document directly. These changes improve browser compatibility and are more efficient in implementing single sign-on procedures.

--- controllers/services/Timer.js
+++ controllers/services/Timer.js
@@ -1,35 +1,20 @@
-function parseCookies(cookies) {
-    const parsedCookies = {};
-    if (!cookies) {
-        return parsedCookies;
-    }
-    let splitCookies = cookies.split(";");
-    splitCookies = splitCookies.map(splitCookie => splitCookie.trim());
-    splitCookies.forEach(cookie => {
-        const cookieComponents = cookie.split("=");
-        const cookieName = cookieComponents[0].trim();
-        let cookieValue = cookieComponents[1].trim();
-        if (cookieValue === "null") {
-            cookieValue = undefined;
-        }
-        parsedCookies[cookieName] = cookieValue;
-    })
+const CHECK_IF_SESSION_HAS_EXPIRED_URL = `${window.location.origin}/checkIfSessionHasExpired`;
+const LOGOUT_WAS_TRIGGERED_URL = `${window.location.origin}/logoutWasTriggered`;
 
-    return parsedCookies;
-}
-
-const handler = setInterval(() => {
-    let {sessionExpiryTime, logout} = parseCookies(document.cookie);
+window.sessionHandler = setInterval(async () => {
+    let response = await fetch(CHECK_IF_SESSION_HAS_EXPIRED_URL);
+    const sessionHasExpired = await response.text();
+    response = await fetch(LOGOUT_WAS_TRIGGERED_URL);
+    const logout = await response.text();
 
     if(logout === "true"){
         document.body.style.setProperty("visibility","hidden");
         alert("A Single Sign-On (SSO) login process is detected to be in progress across multiple browser instances. If this assessment is incorrect, kindly terminate all browser instances and recommence the login procedure.");
-        logout = parseCookies(document.cookie).logout;
         return;
     }
     document.body.style.setProperty("visibility","visible");
-    if (sessionExpiryTime && parseInt(sessionExpiryTime) < Date.now()) {
-        clearInterval(handler);
+    if (sessionHasExpired === "true") {
+        clearInterval(window.sessionHandler);
         window.disableRefreshSafetyAlert = true;
         window.location = "/logout"
     }
